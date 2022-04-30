396b9b05eacbb911cc2eb505cdeb39af61b41ae7
irsyad alyahya <irsyadalyahya5@gmail.com>
2022-04-30 22:22:09 +0700

    content laravel breeze

---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,137 @@
+@extends('layouts.main')
+@section('indexmain')
+		<!--sofa-collection start -->
+		<section id="pengertian">
+			<div class=" owl-theme" >
+				<div class="sofa-collection collectionbg1">
+					<div class="container">
+						<div class="sofa-collection-txt">
+							<h2>Pengertian</h2>
+							<p>
+								Laravel Breeze adalah Starter kits dari laravel yang mengimplementasikan semua fitur otentikasi termasuk, login,registrasi, reset password,konfirmasi password dan, verifikasi email
+							</p>
+						</div>
+					</div>	
+				</div><!--/.sofa-collection-->
+			</div><!--/.collection-carousel-->
+
+		</section><!--/.sofa-collection-->
+		<!--sofa-collection end -->
+
+        <section id="pengertian">
+			<div class=" owl-theme" >
+				<div class="sofa-collection collectionbg1">
+					<div class="container">
+						<div class="sofa-collection-txt">
+							<h2>Kenapa Harus Laravel Breeze?</h2>
+							<p>
+								Laravel Breeze itu ramah untuk pemula, jadi unutk kalian yang baru menggunakan framework laravel laravel breeze ini adalah pilihan starter kit yang bagus untuk kalian pelajari. Tidak seperti Jetstream, Breeze ini lebih simple dan mudah untuk di kustomisasi
+							</p>
+						</div>
+					</div>	
+				</div><!--/.sofa-collection-->
+			</div><!--/.collection-carousel-->
+
+		</section><!--/.sofa-collection-->
+		<!--sofa-collection end -->
+
+	
+
+		<!--blog start -->
+		<section id="blog" class="blog">
+			<div class="container">
+				<div class="section-header">
+					<h2>Fitur-Fitur</h2>
+				</div><!--/.section-header-->
+				<div class="blog-content">
+					<div class="row">
+						<div class="col-sm-3">
+							<div class="single-blog">
+								<div class="single-blog-img">
+									<img src="images/blog/b1.jpg" alt="blog image">
+									<div class="single-blog-img-overlay"></div>
+								</div>
+								<div class="single-blog-txt">
+									<h2>Otentikasi</h2>
+									
+									<p>Dalam Website Modern sistem registrasi sudah umum dilakukan, di laravel breeze kalian bisa membuat sistem ini dengan cepat & mudah. kalian tidak usah repot repot membuatnya sendiri kalian tinggal mengetikan beberapa command di CLI maka laravel dengan sendirinya membuatkanya</p>
+								</div>
+							</div>
+							
+						</div>
+						<div class="col-sm-3">
+							<div class="single-blog">
+								<div class="single-blog-img">
+									<img src="images/blog/b2.jpg" alt="blog image">
+									<div class="single-blog-img-overlay"></div>
+								</div>
+								<div class="single-blog-txt">
+									<h2>Breeze & React / Vue</h2>
+									
+								<p>Laravel breeze juga menawarkan implementasi dari frontend baik itu Vue ataupun React melalui Inertia.js. Ini memungkinkan kamu bisa membuat aplikasi React ataupun Vue dalam satu halaman menggunakan routing dan controller klasik di sisi server</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="single-blog">
+								<div class="single-blog-img">
+									<img src="images/blog/b3.jpg" alt="blog image">
+									<div class="single-blog-img-overlay"></div>
+								</div>
+								<div class="single-blog-txt">
+									<h2>Breeze & Next / API</h2>
+									
+									<p>Laravel Breeze juga dapat mengimplementasikan API otentikasi yang siap untuk mengotentikasi aplikasi JavaScript modern seperti yang didukung oleh Next, Nuxt, dan lainnya. </p>
+								</div>
+							</div>
+						</div>
+                        <div class="col-sm-3">
+							<div class="single-blog">
+								<div class="single-blog-img">
+									<img src="images/blog/b3.jpg" alt="blog image">
+									<div class="single-blog-img-overlay"></div>
+								</div>
+								<div class="single-blog-txt">
+									<h2>Tailwind CSS</h2>
+									
+									<p>Tailwind css adalah css framework yang memiliki design simple dan memprioritaskan proses untuk membangun desaign antarmuka khusus dengan cepat. Di laravel secara default(bawaan) tailwind menjadi pilihan css framework, tetapi kalian juga bisa menggantinya dengan css framework yang lain. tetapi kalian harus mengatur terlebih dahulu</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div><!--/.container-->
+			
+		</section><!--/.blog-->
+		<!--blog end -->
+
+	
+		<!--newsletter strat -->
+		<section id="newsletter"  class="newsletter">
+			<div class="container">
+				<div class="hm-footer-details">
+					<div class="row">
+						<div class=" col-md-3 col-sm-6 col-xs-12">
+							<div class="hm-footer-widget">
+								<div class="hm-foot-title">
+									<h4>information</h4>
+                                    
+								</div><!--/.hm-foot-title-->
+								<div class="hm-foot-menu">
+									<ul>
+										<li><a href="https://youtu.be/CWjCKSmX03Q">Tutorial Youtube</a></li><!--/li-->
+										<li><a href="https://masteringbackend.com/posts/laravel-breeze-tutorial-the-definitive-guide/"> Tutorial Website</a></li><!--/li-->
+										<li><a href="https://laravel.com/docs/9.x/starter-kits#laravel-breeze">Dokumentasi</a></li><!--/li-->
+										<li><a href="#">Github Repo</a></li><!--/li-->
+									</ul><!--/ul-->
+								</div><!--/.hm-foot-menu-->
+							</div><!--/.hm-footer-widget-->
+						</div><!--/.col-->
+					</div><!--/.row-->
+				</div><!--/.hm-footer-details-->
+
+			</div><!--/.container-->
+
+		</section><!--/newsletter-->	
+		<!--newsletter end -->
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,201 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+    <head>
+        <!-- meta data -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+        <!--font-family-->
+		<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+        
+        <!-- title of site -->
+        <title>Laravel Breeze</title>
+
+        <!-- For favicon png -->
+		<link rel="shortcut icon" type="image/icon" href="logo/favicon.png"/>
+       
+        <!--font-awesome.min.css-->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+
+        <!--linear icon css-->
+		<link rel="stylesheet" href="css/linearicons.css">
+
+		<!--animate.css-->
+        <link rel="stylesheet" href="css/animate.css">
+
+        <!--owl.carousel.css-->
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+		<link rel="stylesheet" href="css/owl.theme.default.min.css">
+		
+        <!--bootstrap.min.css-->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		
+		<!-- bootsnav -->
+		<link rel="stylesheet" href="css/bootsnav.css" >	
+        
+        <!--style.css-->
+        <link rel="stylesheet" href="css/style.css">
+        
+        <!--responsive.css-->
+        <link rel="stylesheet" href="css/responsive.css">
+        
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		
+        <!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+	
+	<body>
+		<!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+		
+		
+	
+		<!--welcome-hero start -->
+		<header id="home" class="welcome-hero">
+
+			<div id="header-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+
+				<!--/.carousel-inner -->
+				<div class="carousel-inner" role="listbox">
+					<!-- .item -->
+					<div class="item active">
+						<div class="single-slide-item slide1">
+							<div class="container">
+								<div class="welcome-hero-content">
+									<div class="row">
+										<div class="col-sm-7">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-txt">
+													<h4>Laravel Breeze</h4>
+													<h2>Apa itu Laravel Breeze?</h2>
+													<p>
+														Laravel Breeze adalah starter kits bawaan laravel yang bisa mempermudah kamu untuk membuat sistem Autentikasi sederhana dengan cepat
+													</p>
+													<button class="btn-cart welcome-add-cart" onclick="window.location.href='/register'">
+														
+														Live Demo
+													</button>
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+										<div class="col-sm-5">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-img">
+													<img src="images/breeze.png" alt="slider image">
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+									</div><!--/.row-->
+								</div><!--/.welcome-hero-content-->
+							</div><!-- /.container-->
+						</div><!-- /.single-slide-item-->
+
+					</div><!-- /.item .active-->
+				</div><!-- /.carousel-inner-->
+
+			</div><!--/#header-carousel-->
+
+			<!-- top-area Start -->
+			<div class="top-area">
+				<div class="header-area">
+					<!-- Start Navigation -->
+				    <nav class="navbar navbar-default bootsnav  navbar-sticky navbar-scrollspy"  data-minus-value-desktop="70" data-minus-value-mobile="55" data-speed="1000">
+
+				        <div class="container">            
+				            <!-- Start Atribute Navigation -->
+				            <!-- End Atribute Navigation -->
+
+				            <!-- Start Header Navigation -->
+				            <div class="navbar-header">
+				                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+				                    <span>|||||</span>
+				                </button>
+				                <a class="navbar-brand" href="index.html">Laravel Breeze</a>
+
+				            </div><!--/.navbar-header-->
+				            <!-- End Header Navigation -->
+
+				            <!-- Collect the nav links, forms, and other content for toggling -->
+				            <div class="collapse navbar-collapse menu-ui-design" id="navbar-menu">
+				                <ul class="nav navbar-nav navbar-center" data-in="fadeInDown" data-out="fadeOutUp">
+				                    <li class=" scroll active"><a href="#home">Beranda</a></li>
+                                    <li class=" scroll active"><a href="#pengertian">Tentang</a></li>
+				                    <li class="scroll"><a href="#blog">Fitur</a></li>
+				                    <li class="scroll"><a href="#newsletter">Sumber</a></li>
+				                </ul><!--/.nav -->
+				            </div><!-- /.navbar-collapse -->
+				        </div><!--/.container-->
+				    </nav><!--/nav-->
+				    <!-- End Navigation -->
+				</div><!--/.header-area-->
+			    <div class="clearfix"></div>
+
+			</div><!-- /.top-area-->
+			<!-- top-area End -->
+
+		</header><!--/.welcome-hero-->
+		<!--welcome-hero end -->
+        @yield('indexmain')
+
+		<!--footer start-->
+		<footer id="footer"  class="footer">
+			<div class="container">
+				<div class="hm-footer-copyright text-center">
+					<div class="footer-social">
+						<a href="#"><i class="fa fa-facebook"></i></a>	
+						<a href="#"><i class="fa fa-instagram"></i></a>
+						<a href="#"><i class="fa fa-linkedin"></i></a>
+						<a href="#"><i class="fa fa-pinterest"></i></a>
+						<a href="#"><i class="fa fa-behance"></i></a>	
+					</div>
+					<p>
+						&copy;copyright. designed and developed by <a href="https://www.themesine.com/">themesine</a>
+					</p><!--/p-->
+				</div><!--/.text-center-->
+			</div><!--/.container-->
+
+			<div id="scroll-Top">
+				<div class="return-to-top">
+					<i class="fa fa-angle-up " id="scroll-top" data-toggle="tooltip" data-placement="top" title="" data-original-title="Back to Top" aria-hidden="true"></i>
+				</div>
+				
+			</div><!--/.scroll-Top-->
+			
+        </footer><!--/.footer-->
+		<!--footer end-->
+		
+		<!-- Include all js compiled plugins (below), or include individual files as needed -->
+
+		<script src="js/jquery.js"></script>
+        
+        <!--modernizr.min.js-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+		
+		<!--bootstrap.min.js-->
+        <script src="js/bootstrap.min.js"></script>
+		
+		<!-- bootsnav js -->
+		<script src="js/bootsnav.js"></script>
+
+		<!--owl.carousel.js-->
+        <script src="js/owl.carousel.min.js"></script>
+
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+		
+        
+        <!--Custom JS-->
+        <script src="js/custom.js"></script>
+        
+    </body>
+	
+</html>
